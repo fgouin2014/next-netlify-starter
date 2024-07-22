5b246ad7f43b4fa31121efcd592ff2d022eb0dbc
fgouin2014 <fgouin2014@gmail.com>
2024-07-22 11:11:43 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
-};
-
-module.exports = nextConfig;
+module.exports = {
+  target: 'serverless',
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    })
+    return config
+  },
+}
